4d0513d1bcd9cd1add469415db983b3c388c3101
Miguel Pinto <miguel.pinto@lookatitude.com>
2017-04-28 21:53:21 +0100

    added firebase and the first modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AddImageComponent } from './add-image/add-image.component';
+import { EditImageComponent } from './edit-image/edit-image.component';
+import { ListImagesComponent } from './list-images/list-images.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AddImageComponent,
+    EditImageComponent,
+    ListImagesComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
